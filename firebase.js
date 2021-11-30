16be0b21e9bc6a58c5971e6cea9a0e9559fb54a2
Neh Joshi <nehjoshi5@gmail.com>
2021-11-30 22:10:35 +0530

    Boilerplate

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyACvynLPNY-4VUZIaE5LlhVquDrixCXcaQ",
+    authDomain: "signal-clone-f1de2.firebaseapp.com",
+    projectId: "signal-clone-f1de2",
+    storageBucket: "signal-clone-f1de2.appspot.com",
+    messagingSenderId: "956326899486",
+    appId: "1:956326899486:web:2a869ff512d6873795a30f"
+};
